4f3163a99afebcca19ace9ffd2e3efe427793b68
Alexim <aleximmaestropro@gmail.com>
2023-01-09 02:23:17 +0700

    hm3

---
+++ HM/3/Program.cs
@@ -0,0 +1,57 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// Console.WriteLine("Введите число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+// string str = num.ToString();
+// int len = str.Length;
+// if (len == 5)
+// {
+//     if (str[0] == str[4] && str[1] == str[3])
+//     {
+//         Console.WriteLine("является палиндромом");
+//     }
+//     else
+//     {
+//         Console.WriteLine("не является палиндромом");
+//     }
+// }
+// else
+// {
+//     Console.WriteLine("Введено не пятизначное число");
+// }
+
+
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// Console.WriteLine("Введите x1: ");
+// double x1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите x2: ");
+// double x2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите y1: ");
+// double y1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите y2: ");
+// double y2 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите y2: ");
+// double z1 = Convert.ToDouble(Console.ReadLine());
+// Console.WriteLine("Введите y2: ");
+// double z2 = Convert.ToDouble(Console.ReadLine());
+
+
+// double A = x2 - x1;
+// double B = y2 - y1;
+// double C = z1 - z2;
+
+// double length = Math.Sqrt(A * A + B * B + C * C);
+// Console.WriteLine($"Длинна отрезка {length}");
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+
+// Console.WriteLine("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+
+// for (int i = 1; i <= number; i++)
+// { 
+//     Console.WriteLine($"Таблица квадратов: {i*i*i} ");
+// }
